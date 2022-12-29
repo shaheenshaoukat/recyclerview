8904446d2f848a8cdbecae3838b2699d80f795a3
Administrator <shaheenshoukat047@gmail.com>
2022-12-29 10:32:44 -0800

    CRUD Operation Recyclerviewcomplte

--- app/src/main/java/com/example/cardviewrecycler/adapter/novaladapter.java
+++ app/src/main/java/com/example/cardviewrecycler/adapter/novaladapter.java
@@ -1,4 +1,142 @@
 package com.example.cardviewrecycler.adapter;
 
-public class novaladapter {
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cardviewrecycler.R;
+import com.example.cardviewrecycler.modelclass.novalmodelclass;
+
+import java.util.ArrayList;
+
+public class novaladapter extends RecyclerView.Adapter<novaladapter.myviewholder> {
+
+
+    ArrayList<novalmodelclass> List;
+    Context context;
+
+
+    public novaladapter(ArrayList<novalmodelclass> list, Context context) {
+        List = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false);
+        return new myviewholder(v);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull myviewholder holder, @SuppressLint("RecyclerView") int position) {
+
+        final novalmodelclass myMovieDataList = List.get(position);
+        holder.textViewName.setText(myMovieDataList.getMynovalname());
+        holder.textViewDate.setText(myMovieDataList.getNovaldata());
+        holder.novalImage.setImageResource(myMovieDataList.getNovalimage());
+
+        holder.layout.setBackgroundColor(Color.parseColor(myMovieDataList.getColor()));
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.dialog);
+                EditText edname = dialog.findViewById(R.id.name);
+                EditText eddetails = dialog.findViewById(R.id.details);
+                Button btn = dialog.findViewById(R.id.btn);
+                TextView txt = dialog.findViewById(R.id.text);
+
+                btn.setText("update");
+                txt.setText("Update Data");
+
+                edname.setText(myMovieDataList.getMynovalname());
+                eddetails.setText(myMovieDataList.getNovaldata());
+
+
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        List.set(position, new novalmodelclass(edname.getText().toString(), eddetails.getText().toString(), R.drawable.img_5,"#E91E63"));
+                        notifyItemChanged(position);
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+
+            }
+        });
+
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+
+                AlertDialog.Builder builder=new AlertDialog.Builder(context)
+                        .setTitle("Deleted").
+                        setMessage("Are you Sure deleted massege").
+                        setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                List.remove(position);
+                                notifyItemRemoved(position);
+
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+
+                            }
+                        });
+                builder.show();
+
+                return false;
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return List.size();
+    }
+
+    public class myviewholder extends RecyclerView.ViewHolder {
+
+        ImageView novalImage;
+        TextView textViewName;
+        TextView textViewDate;
+       CardView layout;
+
+        public myviewholder(@NonNull View itemView) {
+            super(itemView);
+
+            novalImage = itemView.findViewById(R.id.imageview);
+            textViewName = itemView.findViewById(R.id.textName);
+            textViewDate = itemView.findViewById(R.id.textdate);
+            layout = itemView.findViewById(R.id.linearlayout);
+        }
+    }
 }
--- app/src/main/java/com/example/cardviewrecycler/recyclerActivity.java
+++ app/src/main/java/com/example/cardviewrecycler/recyclerActivity.java
@@ -1,14 +1,91 @@
 package com.example.cardviewrecycler;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.cardviewrecycler.adapter.novaladapter;
+import com.example.cardviewrecycler.databinding.ActivityMainBinding;
+import com.example.cardviewrecycler.databinding.ActivityRecyclerBinding;
+import com.example.cardviewrecycler.modelclass.novalmodelclass;
+
+import java.util.ArrayList;
 
 public class recyclerActivity extends AppCompatActivity {
+
+
+    ActivityRecyclerBinding binding;
+    ArrayList<novalmodelclass> List = new ArrayList<>();
+    novaladapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler);
+        binding = ActivityRecyclerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        List.add(new novalmodelclass("Alif", "umera ahmad Writer", R.drawable.img,"#FF5722"));
+        List.add(new novalmodelclass("Namal", "Nimra ahmad writer", R.drawable.img_1,"#BA0932" ));
+        List.add(new novalmodelclass("Janat ky paty", "Nimra Ahmad", R.drawable.img_2,"#2A3887"));
+        List.add(new novalmodelclass("peer a Kamil", "Umera Ahmad", R.drawable.img_3,"#CDDC39" ));
+        List.add(new novalmodelclass("Fateh", "Nimra Ahmad", R.drawable.img_4,"#FF018786" ));
+
+
+        LinearLayoutManager obj = new LinearLayoutManager(this);
+        binding.recyclerView.setLayoutManager(obj);
+        adapter = new novaladapter(List, recyclerActivity.this);
+        binding.recyclerView.setAdapter(adapter);
+
+
+        binding.add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Dialog dialog = new Dialog(recyclerActivity.this);
+                dialog.setContentView(R.layout.dialog);
+
+
+                EditText mynovalname = dialog.findViewById(R.id.name);
+                EditText novaldata = dialog.findViewById(R.id.details);
+                Button btn_send = dialog.findViewById(R.id.btn);
+                dialog.show();
+
+                btn_send.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+
+                        if (mynovalname.getText().toString().equals("")) {
+                            Toast.makeText(recyclerActivity.this, "fill Name", Toast.LENGTH_SHORT).show();
+                        }
+
+                        if (novaldata.getText().toString().equals("")) {
+                            Toast.makeText(recyclerActivity.this, "fill Details", Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            List.add(new novalmodelclass(mynovalname.getText().toString(), novaldata.getText().toString(), R.drawable.img_5,"#E91E63"));
+                            adapter.notifyItemInserted(List.size() - 1);
+                            binding.recyclerView.scrollToPosition(List.size() - 1);
+                            dialog.dismiss();
+
+                        }
+                    }
+                });
+
+            }
+        });
+
+
     }
+
+
 }
